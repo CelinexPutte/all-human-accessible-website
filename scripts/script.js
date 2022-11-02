8b078c0381ffbd39820ef277655e84fc83000b06
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2022-11-02 10:56:01 +0100

    <nav> toegevoegd en de :hover kleur aangepast in de nav

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const menuButton = document.querySelectorAll(".menu-button");
 menuButton.forEach(button => {
     button.addEventListener('click',()=> {
         const activeItem = button.dataset.item
-        const headerHeight = document.querySelector('header').offsetHeight
+        const headerHeight = document.querySelector('nav').offsetHeight
         const sectionTop = document.getElementById(activeItem).offsetTop
         window.scrollTo({
             top: sectionTop-headerHeight,
