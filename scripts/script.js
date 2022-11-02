ba033ee82b57266f311119d1cebfa555982c7e33
CelinexPutte <112859814+CelinexPutte@users.noreply.github.com>
2022-11-02 09:15:47 +0100

    Scroll functie verbeterd

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-console.log('hoiii')
+const menuButton = document.querySelectorAll(".menu-button");
+
+menuButton.forEach(button => {
+    button.addEventListener('click',()=> {
+        const activeItem = button.dataset.item
+        const headerHeight = document.querySelector('header').offsetHeight
+        const sectionTop = document.getElementById(activeItem).offsetTop
+        window.scrollTo({
+            top: sectionTop-headerHeight,
+            behavior:'smooth'
+        })
+    })
+})
